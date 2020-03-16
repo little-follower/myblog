8dffb91b83528bd804d0714feb2c733808d2afa1
zhangchengqing <zhangchengqing@nipponpaint.com.cn>
2020-03-16 22:45:54 +0800

    构建springboot 启动图标

---
+++ src/main/java/com/myblog/zcqblog/ZcqblogApplication.java
@@ -0,0 +1,13 @@
+package com.myblog.zcqblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcqblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcqblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myblog/zcqblog/ZcqblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.zcqblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcqblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
